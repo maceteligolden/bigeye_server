86fa5f0d2f3cb0215fc9a3cb7c0f9cbc8b77f743
maceteligolden <maceteligolden@gmail.com>
2024-05-17 20:39:29 +0100

    feat:add interfaces export file

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,5 +1,6 @@
 import { IDatabase } from "./database.interface";
 import { ILogger } from "./logger.interface";
+import { IPaginator } from "./paginator.interface";
 import { IServer, ServerConfig, ServerRoute, ServerRouter } from "./server.interface";
 
-export { IDatabase, ILogger, IServer, ServerConfig, ServerRouter, ServerRoute };
+export { IPaginator, IDatabase, ILogger, IServer, ServerConfig, ServerRouter, ServerRoute };
