d16f5c6679eeefb684a085c40a95a8e2bc2b72d7
maceteligolden <maceteligolden@gmail.com>
2024-05-17 20:07:11 +0100

    feat:add database export file

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,4 +1,5 @@
+import { IDatabase } from "./database.interface";
 import { ILogger } from "./logger.interface";
 import { IServer, ServerConfig, ServerRoute, ServerRouter } from "./server.interface";
 
-export { ILogger, IServer, ServerConfig, ServerRouter, ServerRoute };
+export { IDatabase, ILogger, IServer, ServerConfig, ServerRouter, ServerRoute };
