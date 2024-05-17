43a6c472cd8a4d2e8bccee2cc8cc612dfe776656
maceteligolden <maceteligolden@gmail.com>
2024-05-17 20:34:08 +0100

    feat:add error middleware to server middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import "reflect-metadata";
 import express from "express";
 import "dotenv/config";
-import { Server } from "./shared/helper";
-import { cors } from "./shared/middlewares";
+import { Server } from "./shared/helpers";
+import { cors, errorMiddleware } from "./shared/middlewares";
 import { routes } from "./routes";
 
 const app = express();
 const server = new Server(app);
 server.config({
-  middlewares: [express.json(), express.urlencoded({ extended: true }), cors],
+  middlewares: [
+    express.json(), 
+    express.urlencoded({ extended: true }), 
+    cors,
+    errorMiddleware
+  ],
   routes,
 });
 server.start();
