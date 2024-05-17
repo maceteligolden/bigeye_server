25fe06e97b6d772644dfa39994171216cb893c8e
maceteligolden <maceteligolden@gmail.com>
2024-05-17 20:35:54 +0100

    feat:add badrequest error

---
+++ src/shared/errors/bad_request.error.ts
@@ -0,0 +1,20 @@
+import { container } from "tsyringe";
+import { CustomError } from "./custom.error";
+import LoggerService from "../services/logger.service";
+import { StatusCodes } from "../constants";
+
+export default class BadRequestError extends CustomError {
+    statusCode = StatusCodes.BAD_REQUEST;
+    metadata;
+    constructor(message: string, metadata?: {}) {
+        super(message);
+        this.metadata = metadata;
+        Object.setPrototypeOf(this, BadRequestError.prototype);
+    }
+
+    serializeErrors() {
+        const logger = container.resolve(LoggerService);
+        logger.log(`${this.message}`);
+        return [{ message: this.message }];
+    }
+}
