3d6cc6f98ea24c7579873b57530d92a1e4b95dd0
maceteligolden <maceteligolden@gmail.com>
2024-05-25 17:12:11 +0100

    feat:repository file to export file

--- src/shared/repositories/index.ts
+++ src/shared/repositories/index.ts
@@ -1,1 +1,5 @@
-export {};
+import PlanRepository from "./plan.repository";
+import SubscriptionRepository from "./subscription.repository";
+import UserRepository from "./user.repository";
+
+export { UserRepository, PlanRepository, SubscriptionRepository };
