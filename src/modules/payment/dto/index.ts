ef7bb508705e7786d337aa6f32be389919b14b21
maceteligolden <maceteligolden@gmail.com>
2024-05-25 16:52:28 +0100

    feat:add card dto to export file

---
+++ src/modules/payment/dto/index.ts
@@ -0,0 +1,3 @@
+import { AuthorizeCardInput, AuthorizeCardOutput, SaveCardInput, SaveCardOutput } from "./card.dto";
+
+export { SaveCardInput, SaveCardOutput, AuthorizeCardInput, AuthorizeCardOutput };
