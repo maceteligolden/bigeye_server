b69a17d976a607cd8b8f71667a24d66fa8afc993
maceteligolden <maceteligolden@gmail.com>
2024-05-25 16:51:18 +0100

    feat:add cardcontroller file

---
+++ src/modules/payment/controller/card.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { injectable } from "tsyringe";
+import { StatusCodes } from "../../../shared/constants";
+import { Res } from "../../../shared/helper";
+import { CardService } from "../services";
+
+@injectable()
+export default class CardController {
+  constructor(private cardService: CardService) {}
+
+  async authorizeAddCard(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { user } = req.body;
+
+      const response = await this.cardService.authorizeAddCard({ user_id: user.id });
+
+      Res({
+        res,
+        code: StatusCodes.OK,
+        message: "successfully authorized add card",
+        data: response,
+      });
+    } catch (e: any) {
+      next(e);
+    }
+  }
+}
