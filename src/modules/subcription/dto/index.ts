501a95c8322eb99f0d6ad05882968c589aeab622
maceteligolden <maceteligolden@gmail.com>
2024-05-25 16:58:51 +0100

    feat:add subscription dtos to export files

---
+++ src/modules/subcription/dto/index.ts
@@ -0,0 +1,4 @@
+import { CreatePlanOutput } from "./plan.dto";
+import { CreateSubscriptionInput, CreateSubscriptionOutput } from "./subscription.dto";
+
+export { CreatePlanOutput, CreateSubscriptionInput, CreateSubscriptionOutput };
