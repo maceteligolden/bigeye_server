265816b55d7556ccec9895fb4c7a6c29d2399111
maceteligolden <maceteligolden@gmail.com>
2024-05-25 16:49:35 +0100

    feat:add customerauth router file

---
+++ src/modules/auth/routers/customerauth.router.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction, Router } from "express";
+import { container } from "tsyringe";
+import { CustomerAuthController } from "../controllers";
+
+const customerAuthRouter = Router();
+const customerAuthController = container.resolve(CustomerAuthController);
+
+customerAuthRouter.post("/signup", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.signUp(req, res, next),
+);
+customerAuthRouter.post("/confirm-signup", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.confirmSignUp(req, res, next),
+);
+customerAuthRouter.post("/resend-code", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.resendSignupConfirmationCode(req, res, next),
+);
+customerAuthRouter.post("/signin", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.signIn(req, res, next),
+);
+customerAuthRouter.post("/signout", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.signOut(req, res, next),
+);
+customerAuthRouter.post("/forgotpassword", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.forgotpassword(req, res, next),
+);
+customerAuthRouter.post("/confirm-forgotpassword", (req: Request, res: Response, next: NextFunction) =>
+  customerAuthController.confirmForgotpassword(req, res, next),
+);
+
+export default customerAuthRouter;
