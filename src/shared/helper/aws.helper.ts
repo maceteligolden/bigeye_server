deb326c7f974b3f0ffe239eaa830d8fdbb18d0c1
maceteligolden <maceteligolden@gmail.com>
2024-06-03 16:21:13 +0100

    chore:format code

--- src/shared/helper/aws.helper.ts
+++ src/shared/helper/aws.helper.ts
@@ -1,21 +1,22 @@
 import { injectable } from "tsyringe";
 import jwt from "jsonwebtoken";
 import jwksClient from "jwks-rsa";
-import crypto from "crypto"
+import crypto from "crypto";
 
 @injectable()
 export default class AWS {
   constructor() {}
 
   generateSecretHash(email: string): string {
-    return crypto.createHmac('SHA256', `${process.env.AWS_COGNITO_SECRETHASH}`)
-        .update(`${email}${process.env.AWS_COGNITO_CLIENT_ID}`)
-        .digest('base64');
+    return crypto
+      .createHmac("SHA256", `${process.env.AWS_COGNITO_SECRETHASH}`)
+      .update(`${email}${process.env.AWS_COGNITO_CLIENT_ID}`)
+      .digest("base64");
   }
 
-  async getKey(header: any, callback: any){
+  async getKey(header: any, callback: any) {
     const client = jwksClient({
-      jwksUri: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}/.well-known/jwks.json`
+      jwksUri: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}/.well-known/jwks.json`,
     });
 
     client.getSigningKey(header.kid, (err, key) => {
@@ -26,20 +27,25 @@ export default class AWS {
         callback(null, signingKey);
       }
     });
-  };
+  }
 
   verifySecret(token: string): any {
     return new Promise((resolve, reject) => {
-      jwt.verify(token, this.getKey, {
-        algorithms: ['RS256'],
-        issuer: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}/.well-known/jwks.json`
-      }, (err, decoded) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(decoded);
-        }
-      });
+      jwt.verify(
+        token,
+        this.getKey,
+        {
+          algorithms: ["RS256"],
+          issuer: `https://cognito-idp.${process.env.AWS_COGNITO_REGION}.amazonaws.com/${process.env.AWS_COGNITO_USER_POOL_ID}/.well-known/jwks.json`,
+        },
+        (err, decoded) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(decoded);
+          }
+        },
+      );
     });
   }
 }
