4106ddd206b4baf04b8ab482e5df65835275f7bc
maceteligolden <maceteligolden@gmail.com>
2024-06-07 07:22:59 +0100

    update:add error to response abstraction

--- src/shared/helper/response.helper.ts
+++ src/shared/helper/response.helper.ts
@@ -1,10 +1,11 @@
 import { ServerResponse } from "../interfaces";
 
 export function Res(args: ServerResponse): void {
-  const { res, code, message, data } = args;
+  const { res, code, message, error, data } = args;
 
   res.status(code).json({
     message,
+    error,
     data,
   });
 }
