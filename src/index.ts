9b2e7898d2d179eae2a69e91ff05ee942ccd8177
maceteligolden <maceteligolden@gmail.com>
2024-06-07 07:21:36 +0100

    update:remove errormiddleware from middleware

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import "reflect-metadata";
 import express from "express";
 import "dotenv/config";
 import { Server } from "./shared/facade";
-import { cors, errorMiddleware } from "./shared/middlewares";
+import { cors } from "./shared/middlewares";
 import { routes } from "./routes";
 import fileUpload from "express-fileupload";
 
 const app = express();
 const server = new Server(app);
 server.config({
-  middlewares: [express.json(), express.urlencoded({ extended: true }), fileUpload(), cors, errorMiddleware],
+  middlewares: [express.json(), express.urlencoded({ extended: true }), fileUpload(), cors],
   routes,
 });
 server.start();
